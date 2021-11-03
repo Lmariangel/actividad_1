49bf6e04ac4c2452cd30a21f2352c69f7085733c
Lmariangel <lisettemariangel@gmail.com>
2021-11-02 23:41:41 -0300

    subiendo codigo a github

---
+++ comentario.py
@@ -0,0 +1,8 @@
+#esto es un comentario
+#aprendiendo a declarar variables
+var = "hola mundo lisette"
+var_1 = "99"
+#aprendiendo a imprimir una variable
+print(var)
+#aprendiendo a imprimir un mensaje en python
+print("este es un mensaje en python utilizando print")
